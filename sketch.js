5e4b5bd8ae251ac14dd06ff824e7b465d276f3fe
LaughingAbe <95583062+LaughingAbe@users.noreply.github.com>
2023-06-11 20:59:32 +0200

    Added display and play buttons, note loading

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
 let count = 0;
-let step = 110;
-let sizeb = step;
+let stepSize = 110;
+let buttonSize = stepSize;
 
 let noteNum = [];
 let noteTimes = [];
-let durationb = 6;
-let buttonY = 0;
+let durationMultiplier = 6;
+let buttonY = 200;
 
 //Used to preload our audio in preload
 let audio = [];
 
 //Our sound button definitions
-let sounds = [
+let soundButtons = [
 	{
 		color: "red",
 		note: "Do",
@@ -63,11 +63,23 @@ let sounds = [
 		audio: "E1",
 	},
 ];
+
+// Array of notes and durations
+let notes = [
+	{ note: "C", duration: 1 },
+	{ note: "D", duration: 0.5},
+	{ note: "E", duration: 2 },
+	{ note: "C", duration: 1 },
+	{ note: "D", duration: 2 },
+	{ note: "E", duration: 3 },
+	// Add more notes and durations as needed
+  ];
+
 //This runs before setup
 function preload() {
 	//Preload our wav files from assets folder
-	for (let i = 0; i < sounds.length; i++) {
-		audio[i] = loadSound("assets/" + sounds[i]["audio"] + ".wav");
+	for (let i = 0; i < soundButtons.length; i++) {
+		audio[i] = loadSound("assets/" + soundButtons[i]["audio"] + ".wav");
 	}
 }
 
@@ -76,28 +88,61 @@ function setup() {
 	createCanvas(1024, 768);
 	background(255);
 
-	for (let i = 0; i < sounds.length; i++) {
-		createButtonb(i, sounds[i]["color"], sounds[i]["note"]);
+	for (let i = 0; i < soundButtons.length; i++) {
+		createButtonb(i, soundButtons[i]["color"], soundButtons[i]["note"]);
 	}
+
+	createButtonc("Display", displayNotes);
+  	createButtonc("Play", playNotes);
 }
 
 //This is the main program loop
-function draw() {}
+function draw() {
+	// Display the notes on the screen
+	for (let i = 0; i < noteNum.length; i++) {
+		let xPos = i * buttonSize;
+		let yPos = buttonY + buttonSize;
+	
+		fill(0);
+		textAlign(CENTER, CENTER);
+		text(noteNum[i], xPos + buttonSize / 2, yPos + buttonSize / 2);
+	}
+}
 
 function createButtonb(index, color, note) {
-	let xPos = index * sizeb;
+	let xPos = index * buttonSize;
 	let yPos = buttonY;
 
 	let button = createButton(note);
 	button.position(xPos, yPos);
-	button.size(sizeb, sizeb);
+	button.size(buttonSize, buttonSize);
 	button.style("background-color", color);
 	button.mousePressed(() => {
 		audio[index].play();
 		noteNum.push(index + 1);
 		noteTimes.push(count);
-		count += step;
+		count += stepSize;
 	});
+}
+
+function createButtonc(label, onClick) {
+	let button = createButton(label);
+	button.position(100, height - 450);
+	button.mousePressed(onClick);
+}
+
+function displayNotes() {
+	console.log("Notes:", noteNum);
+}
+  
+function playNotes() {
+	for (let i = 0; i < noteNum.length; i++) {
+	  let noteIndex = noteNum[i] - 1;
+	  let durationb = notes[noteIndex].duration;
+	  setTimeout(() => {
+		audio[noteIndex].play();
+	  }, durationb * 1000);
+	}
 }
 
 function keyPressed() {
@@ -105,10 +150,10 @@ function keyPressed() {
 		// Spacebar
 		noteNum.push(0);
 		noteTimes.push(0);
-		count += step;
+		count += stepSize;
 	} else if (keyCode === BACKSPACE) {
 		noteNum.pop();
 		noteTimes.pop();
-		count -= step;
+		count -= stepSize;
 	}
 }
